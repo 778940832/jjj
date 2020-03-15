197f8556aa8f11faa14cdcf0ffcf10a071e04835
778940832 <62177004+778940832@users.noreply.github.com>
2020-03-15 21:58:39 +0800

    Update wea.js

--- wea.js
+++ wea.js
@@ -6,7 +6,7 @@ let config = {
     darksky_api: "4ac793194a07a8b14fbea00aa2cf8d17", //从https://darksky.net/dev/ 上申请key填入即可
     aqicn_api: "2015dfcc7801724696837dc6be7d1d8eaaf9663c", //从http://aqicn.org/data-platform/token/#/ 上申请key填入即可
     huweather_apiKey: "22def88143eb4df185aa4123eaf93277", //和风天气APIkey,可自行前往 https://dev.heweather.com/ 进行获取(注意key类型选WebApi)
-    lat_lon: "39.958087,116.428415", //请填写经纬度,直接从谷歌地图中获取即可
+    lat_lon: "27.097286, 119.650495", //请填写经纬度,直接从谷歌地图中获取即可
     lang: "zh", //语言,随意切换为您想要的语言哦(zh/zh-tw/ja/en/fr/...)
     log: 1, //调试日志,0为不开启,1为开启,2为开启精简日志
     timeout: 0, //超时时间,单位毫秒(1000毫秒=1秒),一般不推荐修改[为0则不限制超时时间]
